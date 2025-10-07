e92b6f7d286645dfd3db09a08a1823529c17cf1e
KillerQueen59 <fauzanramadhan59@gmail.com>
2025-10-07 12:20:37 +0700

    adjust map

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  optimizeFonts: false, // Disable font optimization to avoid build-time font fetching issues
+  optimizeFonts: false,
 };
 
 module.exports = nextConfig;
